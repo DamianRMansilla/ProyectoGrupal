e82d3ceaa2c34a3c25340e0837ec8610cbd82f35
DamianRMansilla <damianrmansilla85@gmail.com>
2022-02-12 16:38:01 +0100

    commit requerido por python

--- Proyecto/AppCoder/forms.py
+++ Proyecto/AppCoder/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 class FormCurso(forms.Form):
     grado = forms.IntegerField()
     division = forms.CharField()
@@ -21,3 +24,23 @@ class FormDirectivo(forms.Form):
     apellido = forms.CharField()
     dni = forms.IntegerField()
     
+class FormDirectivo(forms.Form):
+    nombre = forms.CharField()
+    apellido = forms.CharField()
+    dni = forms.IntegerField()
+
+
+
+
+
+class UserEditForm(UserCreationForm):
+
+    first_name = forms.CharField(label="Nombre")
+    last_name = forms.CharField(label="Apellido")
+    email = forms.EmailField(label="Modificar email")
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repetir la contraseña', widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = [ 'first_name', 'last_name', 'email', 'password1', 'password2' ]
